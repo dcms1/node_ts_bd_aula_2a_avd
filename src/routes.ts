4d344d3e33d4b6ce99eb00f7d0243e0e3886ec8d
Daniel Milagres <66143702+dcms1@users.noreply.github.com>
2021-06-17 20:00:44 -0300

    Add files via upload

--- src/routes.ts
+++ src/routes.ts
@@ -1,27 +1,36 @@
 import { Router } from 'express'
 
-import { ResponsavelController } from './controllers/ResponsavelController'
+import { ClientsController } from './controllers/ClientsController'
 
-import { ServicesController } from './controllers/DespesaController'
+import { ServicesController } from './controllers/ServicesController'
 
 import { OrdermServicoController } from './controllers/OrdemServicoController'
 
 const routes = Router();
 
-const responsavelController = new ResponsavelController()
+const clientsController = new ClientsController()
 
 const servicesController = new ServicesController()
 
 const ordemServicoController = new OrdermServicoController()
 
-routes.post('/responsavel', responsavelController.create)
-routes.get('/responsavel', responsavelController.index)
+routes.post('/clients', clientsController.create)
+routes.get('/clients', clientsController.index)
+routes.get('/clients/:id', clientsController.show)
+routes.delete('/clients/:id', clientsController.delete)
+routes.put('/clients/:id', clientsController.update)
 
 routes.post('/services', servicesController.create)
 routes.get('/services', servicesController.index)
 routes.get('/services/:id', servicesController.show)
 routes.delete('/services/:id', servicesController.delete)
 routes.put('/services/:id', servicesController.update)
+
+routes.post('/ordemservico', ordemServicoController.create)
+routes.get('/ordemservico', ordemServicoController.index)
+routes.get('/ordemservico/:id', ordemServicoController.show)
+routes.delete('/ordemservico/:id', ordemServicoController.delete)
+routes.put('/ordemservico/:id', ordemServicoController.update)
 
 export { routes }
 
